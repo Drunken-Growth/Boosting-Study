9e8ee23e8fbdcc5b2e3d6e7e8d5b7ed21a647846
Juhye-Kim <kjh960215@gmail.com>
2021-06-30 19:25:12 +0900

    solve 캐시

--- 김주혜/210630/캐시.js
+++ 김주혜/210630/캐시.js
@@ -1,3 +1,16 @@
+/* 프로그래머스 - 캐시
+    1. cities를 순회 (순회대상 = city)
+    2. cache에 있는지 확인 (addTime 함수)
+        - hitIdx : 캐시에서 찾은 위치 index
+      2-1. 있으면(hitIdx>0) 
+            1) 총시간 + 5 
+            2) 캐시에서 그 값을 최근값으로 갱신
+      2-2. 없으면(else)
+            1) 총시간 + 1
+            2) 캐시에서 하나 빼고, 하나 넣기
+    3. 총시간 리턴
+*/
+
 function solution(cacheSize, cities) {
   let cache = [];
   let totalTime = 0;
