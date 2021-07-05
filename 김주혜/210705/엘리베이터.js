b7252348300b6ecee2f621614877e81c8c0743a3
Juhye-Kim <kjh960215@gmail.com>
2021-07-05 17:34:24 +0900

    solve 엘리베이터

--- 김주혜/210705/엘리베이터.js
+++ 김주혜/210705/엘리베이터.js
@@ -1,5 +1,4 @@
 // 2번. 엘리베이터
-// cost 순회하면서 최대 연속 부분집합 찾기
 
 function solution(money, cost) {
   let max = 0;
@@ -10,8 +9,8 @@ function solution(money, cost) {
     sum += cost[i];
     if (sum > money) {
       max = Math.max(max, cnt);
-      [cnt, sum] = [0, 0];
       i -= cnt - 1;
+      [cnt, sum] = [0, 0];
     } else {
       cnt++;
       i++;
@@ -25,3 +24,4 @@ console.log(solution(420, [0, 900, 0, 200, 150, 0, 30, 50])); // 5
 console.log(solution(100, [245, 317, 151, 192])); // 0
 console.log(solution(10, [1, 2, 1, 2, 3, 4])); // 5
 console.log(solution(15, [1, 2, 1, 2, 3, 4])); // 6
+console.log(solution(20, [10, 10, 5, 5])); // 2
