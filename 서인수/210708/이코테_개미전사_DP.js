0f0b1c57edf75992963a72b457028e52d2881b9e
Outwater <kocon135@gmail.com>
2021-07-09 10:50:37 +0900

    resolve 개미천사

--- 서인수/210708/이코테_개미전사_DP.js
+++ 서인수/210708/이코테_개미전사_DP.js
@@ -3,7 +3,9 @@
 
 // why DP
 // 1. 큰 문제 작은문제로?  => 흠...
+//   -> 앞에서 부터의 최대값을 하나씩 늘려가며 구한다. (N개의 문제로 쪼갬)
 // 2. 작은 문제가 큰 문제의 변화없이 재사용 하는가? => 흠...
+//    -> i번째의 최대합의 계산은 i-1번째 값과 i-2번째의 값을 필요로 한다.
 
 // 풀이방법 (DP?...그냥 되는대로 풀어버렸다.)
 // 1. foodList 순회, i 번째의 선택여부는 i + i-2 번쨰의합과 i-1번째의 값과 비교한다.
@@ -30,6 +32,23 @@ function solution(N, foodList) {
   return isAttack.reduce((acc, cur) => acc + cur);
 }
 
+// 2차풀이 이코테 책 참고 (바텀업)
+// memo[i] 는 i번째 까지 털 수 있는 최대값을 의미한다.
+
+function solution(N, foodList) {
+  let memo = [];
+  memo[0] = foodList[0];
+  memo[1] = Math.max(foodList[0], foodList[1]);
+
+  // 이전값까지의 합이크면, 지금껀 선택하지 않는다.
+  // (i-1)번까지의 합과 현재값 합친게 더 크면, 현재값을 선택한다.
+  for (let i = 2; i < N; i++) {
+    memo[i] = Math.max(memo[i - 1], memo[i - 2] + foodList[i]);
+  }
+  console.log(memo);
+  return memo[N - 1];
+}
+
 console.log(solution(4, [1, 3, 1, 5]));
-console.log(solution(5, [1, 3, 1, 5, 6]));
-console.log(solution(5, [1, 3, 10, 5, 6]));
+// console.log(solution(5, [1, 3, 1, 5, 6]));
+// console.log(solution(5, [1, 3, 10, 5, 6]));
