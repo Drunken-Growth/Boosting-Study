a75ef815085326dde38f8ce3c995a9843addb2c1
JewonYeon <yjw9852@gmail.com>
2021-07-12 14:01:27 +0900

    solve 코딜리티-FrogJmp

---
+++ 연제원/210712/코딜리티/4. FrogJmp.js
@@ -0,0 +1,12 @@
+// 새로 풀이
+function solution(X, Y, D) {
+  return Math.ceil((Y - X) / D);
+}
+
+// 기존 풀이
+function solution(X, Y, D) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  return Math.ceil((Y - X) / D);
+}
+
+console.log(solution(10, 85, 30)); // 3
